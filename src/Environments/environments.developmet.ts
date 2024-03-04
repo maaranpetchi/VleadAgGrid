919b7d78e114fed6364c49c8779287b5acb664b2
Petchiappan.P <Petchiappan.P@KTP-KIT008>
2024-03-04 10:36:20 +0530

    Quality allocation popup added instruction

--- src/Environments/environments.developmet.ts
+++ src/Environments/environments.developmet.ts
@@ -10,5 +10,5 @@ export const environment = {
   apiURL: 'https://localhost:7208/api/'
 
   ////Live server///
-  //apiURL: 'https://vislocalapi.vleadservices.com/api/'
+   //apiURL: 'https://vislocalapi.vleadservices.com/api/'
 };
